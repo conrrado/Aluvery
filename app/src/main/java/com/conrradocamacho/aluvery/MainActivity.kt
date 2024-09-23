83bd541f1d3db4645d8165639c5a280933876c15
Conrrado Kantoviz Francisco Camacho <conrrado@motorola.com>
2024-09-22 21:59:48 -0300

    Adjustment on ProductItem screen
    - add degrade in header
    - position the image in the center

--- app/src/main/java/com/conrradocamacho/aluvery/MainActivity.kt
+++ app/src/main/java/com/conrradocamacho/aluvery/MainActivity.kt
@@ -14,18 +14,26 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.conrradocamacho.aluvery.ui.theme.AluveryTheme
+import com.conrradocamacho.aluvery.ui.theme.Purple500
+import com.conrradocamacho.aluvery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,15 +52,25 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 private fun ProductItem() {
-    Column {
-        Box(modifier = Modifier
-            .height(50.dp)
-            .width(50.dp)
-            .background(Color.Blue)
+    Column(
+        modifier = Modifier
+            .height(250.dp)
+            .width(200.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .fillMaxWidth()
+                .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
         )
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Image of product"
+            contentDescription = "Image of product",
+            modifier = Modifier
+                .offset(y = (-50).dp)
+                .size(100.dp)
+                .clip(shape = CircleShape)
+                .align(Alignment.CenterHorizontally)
         )
         Text(text = "Title")
         Text(text = "Price")
