f9e0f1c3ad07bcea58b5477a63e810f86bc4d3a7
Conrrado Camacho <con.webmaster@gmail.com>
2024-09-25 22:15:49 -0300

    Adjustment on ProductItem screen
    - adjustment for both texts
    - add shadow and rounded corner for item
    - optimize imports

--- app/src/main/java/com/conrradocamacho/aluvery/MainActivity.kt
+++ app/src/main/java/com/conrradocamacho/aluvery/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.conrradocamacho.aluvery
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,17 +8,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,10 +25,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.conrradocamacho.aluvery.ui.theme.AluveryTheme
 import com.conrradocamacho.aluvery.ui.theme.Purple500
 import com.conrradocamacho.aluvery.ui.theme.Teal200
@@ -41,7 +42,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AluveryTheme {
-                Surface {
+                Surface(
+                    modifier = Modifier
+                        .padding(top = 24.dp)
+                ) {
                     ProductItem()
                 }
             }
@@ -52,28 +56,56 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 private fun ProductItem() {
-    Column(
+    Surface(
         modifier = Modifier
-            .height(250.dp)
-            .width(200.dp)
+            .padding(8.dp),
+        shape = RoundedCornerShape(15.dp),
+        shadowElevation = 4.dp
     ) {
-        Box(
+        Column(
             modifier = Modifier
-                .height(100.dp)
-                .fillMaxWidth()
-                .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Image of product",
-            modifier = Modifier
-                .offset(y = (-50).dp)
-                .size(100.dp)
-                .clip(shape = CircleShape)
-                .align(Alignment.CenterHorizontally)
-        )
-        Text(text = "Title")
-        Text(text = "Price")
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .fillMaxWidth()
+                    .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Image of product",
+                    modifier = Modifier
+                        .offset(y = imageSize / 2)
+                        .size(imageSize)
+                        .clip(shape = CircleShape)
+                        .align(Alignment.BottomCenter)
+                )
+            }
+            Spacer(modifier = Modifier
+                .height(imageSize / 2))
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 14,99",
+                    modifier = Modifier
+                        .padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
+        }
     }
 }
 
