5ec954ff291eb1167ee2aea0a168fefdc7c2cabd
Conrrado Camacho <con.webmaster@gmail.com>
2024-11-03 20:02:23 -0300

    feat: Implement product search functionality
    - Added an OutlinedTextField to HomeScreen for entering search queries.
    - Introduced a state variable to manage the search query text.

--- app/src/main/java/com/conrradocamacho/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/conrradocamacho/aluvery/ui/screens/HomeScreen.kt
@@ -6,33 +6,43 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.conrradocamacho.aluvery.model.Product
-import com.conrradocamacho.aluvery.sampledata.sampleProducts
 import com.conrradocamacho.aluvery.sampledata.sampleSections
 import com.conrradocamacho.aluvery.ui.components.ProductSection
+import com.conrradocamacho.aluvery.ui.theme.AluveryTheme
 
 @Composable
 fun HomeScreen(
     sections: Map<String, List<Product>>,
 ) {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(vertical = 16.dp)
-    ) {
-        sections.forEach {
-            val title = it.key
-            val products = it.value
-            item {
-                ProductSection(title = title, products = products)
+    Column {
+        var text by remember { mutableStateOf("") }
+        OutlinedTextField(value = text, onValueChange = {
+            text = it
+        })
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
+            sections.forEach {
+                val title = it.key
+                val products = it.value
+                item {
+                    ProductSection(title = title, products = products)
+                }
             }
         }
     }
@@ -41,5 +51,17 @@ fun HomeScreen(
 @Preview(showSystemUi = true)
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreen(sampleSections)
+    AluveryTheme {
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+        ) { innerPadding ->
+            Surface(
+                modifier = Modifier
+                    .padding(innerPadding)
+            ) {
+                HomeScreen(sampleSections)
+            }
+        }
+    }
 }
