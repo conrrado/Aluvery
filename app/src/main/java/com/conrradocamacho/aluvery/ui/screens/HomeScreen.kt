721fbeca446d6af663400c659fe9240581e615e0
Conrrado Camacho <con.webmaster@gmail.com>
2024-11-04 21:47:24 -0300

    refactor: Enhance OutlinedTextField styling for search
    - Added a search icon to the OutlinedTextField.
    - Included a label and placeholder for better user guidance.
    - Applied rounded corners to the field for a more polished look.

--- app/src/main/java/com/conrradocamacho/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/conrradocamacho/aluvery/ui/screens/HomeScreen.kt
@@ -4,11 +4,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,9 +34,28 @@ fun HomeScreen(
 ) {
     Column {
         var text by remember { mutableStateOf("") }
-        OutlinedTextField(value = text, onValueChange = {
-            text = it
-        })
+        OutlinedTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(100),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "ícone de busca"
+                )
+            },
+            label = {
+                Text(text = "Produto")
+            },
+            placeholder = {
+                Text(text = "O que você procura?")
+            }
+        )
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize(),
