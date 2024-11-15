f01b2fcc0f150cd09491237bebfd6ff4400560f5
Conrrado Camacho <con.webmaster@gmail.com>
2024-11-15 15:30:15 -0300

    feat: Implement expandable product card
    - Added click functionality to the product card to toggle its expanded state.
    - Introduced a state variable to manage the card's expansion state.

--- app/src/main/java/com/conrradocamacho/aluvery/ui/components/CardProductItem.kt
+++ app/src/main/java/com/conrradocamacho/aluvery/ui/components/CardProductItem.kt
@@ -1,18 +1,25 @@
 package com.conrradocamacho.aluvery.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalFontFamilyResolver
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.Dp
@@ -32,10 +39,14 @@ fun CardProductItem(
     modifier: Modifier = Modifier,
     elevation: Dp = 4.dp
 ) {
+    var isExpanded by remember { mutableStateOf(false) }
     Card(
         modifier = modifier
             .fillMaxWidth()
-            .heightIn(150.dp),
+            .heightIn(150.dp)
+            .clickable {
+                isExpanded = !isExpanded
+            },
         elevation = CardDefaults.cardElevation(
             defaultElevation = elevation
         )
@@ -63,11 +74,15 @@ fun CardProductItem(
                     text = product.price.toBrazilianCurrency()
                 )
             }
+            val textOverflow = if (isExpanded) TextOverflow.Visible else TextOverflow.Ellipsis
+            val maxLines = if (isExpanded) Int.MAX_VALUE else 2
             product.description?.let {
                 Text(
                     text = product.description,
                     Modifier
-                        .padding(16.dp)
+                        .padding(16.dp),
+                    overflow = textOverflow,
+                    maxLines = maxLines,
                 )
             }
         }
